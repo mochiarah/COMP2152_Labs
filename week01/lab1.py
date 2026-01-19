e11656c0580caf6b2f0848647630c7a57c6944ef
mochiarah <kai.mercado1@gmail.com>
2026-01-18 23:38:39 -0500

    add lab1.py

---
+++ week01/lab1.py
@@ -0,0 +1,23 @@
+# Sample Coding Question 01 Week 01
+# Khiara Mercado
+
+# Question 2: Defining an array (list)
+my_array = [1, 4, 7, 9]
+
+# Question 3: Define 4 variables a, b, c, d and give them the values 1, 2, 3, 4
+a = 1
+b = 2
+c = 3
+d = 4
+
+e = (a - ((b ** c) // d)) + (a % c)
+
+# Question 4: Formatting
+temperature = 32.6
+print("The temperature today is: {:.3f} degrees Celsius".format(temperature))
+
+# Question 5:
+userAge = int(input("Enter your age: "))
+
+userAge = userAge + 22
+print("Now showing the shop items filtered by age: ", userAge)
